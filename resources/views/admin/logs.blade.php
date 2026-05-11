bc5822177aabd2160cac51e6e4e95f64306d46cf
Matilda Smets <101006467+issatillie@users.noreply.github.com>
2026-05-11 23:56:15 +0200

    Verwijdering sort dropdown, onnodig met sorting via Flux

--- resources/views/admin/logs.blade.php
+++ resources/views/admin/logs.blade.php
@@ -3,21 +3,7 @@
     :user="user()"
 >
     <x-slot:headerActions>
-        <div class="flex flex-row gap-3">
-            <flux:input class="max-w-xs" placeholder="Search for a log..." icon:trailing="magnifying-glass" />
-
-            <flux:dropdown>
-                <flux:button icon:trailing="chevron-down">Sort by</flux:button>
-                <flux:menu>
-                    <flux:menu.radio.group>
-                        <flux:menu.radio checked value="recent">Recent</flux:menu.radio>
-                        <flux:menu.radio value="oldest">Oldest</flux:menu.radio>
-                        <flux:menu.radio value="az">A-Z (action)</flux:menu.radio>
-                        <flux:menu.radio value="za">Z-A (action)</flux:menu.radio>
-                    </flux:menu.radio.group>
-                </flux:menu>
-            </flux:dropdown>
-        </div>
+        <flux:input class="max-w-xs" placeholder="Search for a log..." icon:trailing="magnifying-glass" />
     </x-slot:headerActions>
 
     <flux:table>
