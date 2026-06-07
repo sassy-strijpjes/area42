f11f459bfeef6f69f15382ae3e8b9e6f29728df3
Matilda Smets <101006467+issatillie@users.noreply.github.com>
2026-06-07 12:00:41 +0200

    Verwerking us-sys-01 en verplaatsing staff routes naar aparte file

---
+++ software/routes/staff.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::name('staff.')->middleware(['auth'])->group(function () {
+    Route::get('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+    Route::get('reset-password/{token}', [AuthController::class, 'resetPassword'])->name('reset-password');
+});
+
+Route::name('staff.')->prefix('staff')->middleware(['auth'])->group(function () {
+    Route::view('/', 'staff.dashboard')->name('dashboard');
+    Route::view('roles', 'staff.roles.index')->middleware('permission:view_roles')->name('roles');
+    Route::view('roles/create', 'staff.roles.create')->middleware('permission:add_roles')->name('roles.create');
+    Route::view('roles/{role}/edit', 'staff.roles.edit')->middleware('permission:edit_roles')->name('roles.edit');
+    Route::view('staff', 'staff.staff.index')->middleware('permission:view_staff')->name('staff');
+    Route::view('staff/create', 'staff.staff.create')->middleware('permission:add_staff')->name('staff.create');
+    Route::view('staff/{staff}/edit', 'staff.staff.edit')->middleware('permission:edit_staff')->name('staff.edit');
+    Route::view('bookings/restaurant', 'staff.bookings.restaurant.index')->middleware('permission:view_restaurant-bookings')->name('restaurant.bookings');
+    Route::view('bookings/restaurant/create', 'staff.bookings.restaurant.create')->middleware('permission:add_restaurant-bookings')->name('restaurant.bookings.create');
+    Route::view('bookings/restaurant/{booking}/edit', 'staff.bookings.restaurant.edit')->middleware('permission:edit_restaurant-bookings')->name('restaurant.bookings.edit');
+    Route::get('login', [AuthController::class, 'type'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
